97bf65fe1a7c96980a97d17b853a7d09086fb3bc
Sidibé Arouna <sidibearouna71@gmail.com>
2025-02-10 14:18:34 +0000

    update dependencies and fix configuration files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      extend: {},
-    },
+    extend: {},
   },
   plugins: [],
 }
-
